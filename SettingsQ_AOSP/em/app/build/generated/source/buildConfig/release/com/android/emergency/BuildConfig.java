6b6228c4ecfbd4442a364aba8272429862a7b094
deqin.tang <deqin.tang@tcl.com>
2019-09-12 17:22:38 +0800

    add_settingsQ_AOSP

---
+++ SettingsQ_AOSP/em/app/build/generated/source/buildConfig/release/com/android/emergency/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.emergency;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.android.emergency";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 201906041;
+  public static final String VERSION_NAME = "v9.1.0.0001.0";
+}
